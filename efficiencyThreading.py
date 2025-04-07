9fc0cbb733313d20fd9a9a6ae21b1d9a91e8f5fb
Prinkal Christian <iprinkal181999@gmail.com>
2025-04-07 18:39:46 +0100

    [Added]threading

---
+++ efficiencyThreading.py
@@ -0,0 +1,28 @@
+import threading
+import time
+from dataCreation import generateData
+from graph import watchSentimentUpdate
+from mongoConnect import connectMongoDB
+from realtimeTracking import watchCrimeUpdate
+
+
+def crimeUpdate():
+    db = connectMongoDB()
+    try:
+        print(db)
+        watchCrimeUpdate(db)
+    except Exception as e:
+        print("error occured while performing this action:", e)
+
+def UpdatingTweets():
+    time.sleep(5)
+    for i in range(100):
+        generateData()
+        time.sleep(1)
+    
+def sentimentUpdate():
+    db=connectMongoDB()
+    try:
+        watchSentimentUpdate(db)
+    except Exception as e:
+        print("error occured while performing this action:", e)
--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-import time
+
 import threading
 from dataCreation import generateData
+from efficiencyThreading import UpdatingTweets, crimeUpdate, sentimentUpdate
 from graph import updateGraph, watchSentimentUpdate
 from mongoConnect import connectMongoDB
 from realtimeTracking import watchCrimeUpdate
-
-def crimeUpdate():
-    db = connectMongoDB()
-    try:
-        print(db)
-        watchCrimeUpdate(db)
-    except Exception as e:
-        print("error occured while performing this action:", e)
-
-def UpdatingTweets():
-    time.sleep(5)
-    for i in range(100):
-        generateData()
-        time.sleep(1)
-    
-def sentimentUpdate():
-    db=connectMongoDB()
-    try:
-        watchSentimentUpdate(db)
-    except Exception as e:
-        print("error occured while performing this action:", e)
-
-
-crimeThread = threading.Thread(target=crimeUpdate)
-tweetThread= threading.Thread(target=UpdatingTweets)
-sentimentThread=threading.Thread(target=sentimentUpdate)
-# graphThread=threading.Thread(target=updateGraph)
 
 if __name__ == "__main__":
   # Start data generation and watchers
